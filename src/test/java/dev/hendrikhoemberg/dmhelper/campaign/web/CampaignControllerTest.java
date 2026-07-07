37deaaf50d02cc7d25bc9118a026fb4db56f08fb
Hendrik Hömberg <hendrik.hoemberg@web.de>
2026-07-07 14:36:40 +0200

    fix: add SrdSeedService/SpellSeedService mocks to CampaignControllerTest

--- src/test/java/dev/hendrikhoemberg/dmhelper/campaign/web/CampaignControllerTest.java
+++ src/test/java/dev/hendrikhoemberg/dmhelper/campaign/web/CampaignControllerTest.java
@@ -2,6 +2,8 @@ package dev.hendrikhoemberg.dmhelper.campaign.web;
 
 import dev.hendrikhoemberg.dmhelper.campaign.data.Campaign;
 import dev.hendrikhoemberg.dmhelper.campaign.service.CampaignService;
+import dev.hendrikhoemberg.dmhelper.library.service.SpellSeedService;
+import dev.hendrikhoemberg.dmhelper.library.service.SrdSeedService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -29,6 +31,12 @@ class CampaignControllerTest {
 
     @MockitoBean
     private CampaignService service;
+
+    @MockitoBean
+    private SrdSeedService srdSeedService;
+
+    @MockitoBean
+    private SpellSeedService spellSeedService;
 
     private Campaign sampleCampaign() {
         Campaign c = new Campaign();
