43d45f0d5bd29e478ad7f77f364d8561a7cc6cc7
Hendrik Hömberg <hendrik.hoemberg@web.de>
2026-07-07 09:22:05 +0200

    Initialize project structure with Maven, add application files and configurations

---
+++ src/main/java/dev/hendrikhoemberg/dmhelper/DmhelperApplication.java
@@ -0,0 +1,13 @@
+package dev.hendrikhoemberg.dmhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmhelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hendrikhoemberg/dmhelper/DmhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hendrikhoemberg.dmhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
